2ebd55019c77b284dc2e7f0f9d2e5de632959ec0
A P <131835703+s81c8f@users.noreply.github.com>
2024-04-09 17:24:52 -0400

    Stylize password match

--- js/script.js
+++ js/script.js
@@ -1,23 +1,43 @@
 function passwordCheck () {
     let passwordInit = document.querySelector("#password");
     let passwordConfirm = document.querySelector("#password-confirm");
+    let passwordInitError = document.querySelector("#password + .error");
+    let passwordConfirmError = document.querySelector("#password-confirm + .error");
     let myForm = document.querySelector("#create-account-form")
 
-    let isPasswordIdentical = false;
-
-    // passwordConfirm.addEventListener("click", function () {
-
-    // })
-    if(passwordConfirm.value !== "") {
-        if (passwordInit.value === passwordConfirm.value) {
-            isPasswordIdentical = true;
-            passwordInit.style["background-color"] = "green";
+    passwordInit.addEventListener("input", function () {
+        if (passwordInit.value.length < 8) {
+            passwordInitError.textContent = "Password must be 8 characters or more";
+            passwordInit.classList.add("input-invalid");
         }
-        else if (passwordInit.value !== passwordConfirm.value) {
-            isPasswordIdentical = false;
-            passwordInit.style["background-color"] = "red";
+        else {
+            passwordInitError.textContent = "";
+            passwordInit.classList.remove("input-invalid");
         }
-    }
+
+        //password matching
+        if (passwordConfirm.value !== "" && 
+            passwordInit.value !== passwordConfirm.value) {
+            passwordConfirmError.textContent = "Passwords do not match";
+            showPasswordInvalid(passwordInit, passwordConfirm)
+        }
+        else if (passwordInit.value === passwordConfirm.value) {
+            passwordConfirmError.textContent = "";
+            showPasswordValid(passwordInit, passwordConfirm);
+        }
+
+    });
+
+    passwordConfirm.addEventListener("input", function () {
+        if (passwordInit.value !== passwordConfirm.value) {
+            passwordConfirmError.textContent = "Passwords do not match";
+            showPasswordInvalid(passwordInit, passwordConfirm);
+        }
+        else {
+            passwordConfirmError.textContent = "";
+            showPasswordValid(passwordInit, passwordConfirm);
+        }
+    });
 
     myForm.addEventListener("submit", function(event){
         event.preventDefault();
@@ -29,6 +49,16 @@ function passwordCheck () {
             alert("Passwords don't match");
         }
     })
+}
+
+function showPasswordInvalid(passwordInit, passwordConfirm) {
+    passwordInit.classList.add("input-invalid");
+    passwordConfirm.classList.add("input-invalid");
+}
+
+function showPasswordValid(passwordInit, passwordConfirm) {
+    passwordInit.classList.remove("input-invalid");
+    passwordConfirm.classList.remove("input-invalid");
 }
 
 passwordCheck();
