beb631c4930a73cae94ecec46f9c969cba3971c2
A P <131835703+s81c8f@users.noreply.github.com>
2024-04-10 14:54:17 -0400

    Tweak email validation regex

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ function isFormValid(passwordInit, passwordConfirm) {
 }
 
 function checkEmail(email){
-    const emailRegex = /^[a-zA-Z0-9\.\_\-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
 
     email.addEventListener("blur", function() {
         let emailInput = email.value;
